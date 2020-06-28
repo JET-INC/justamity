4082621a3ca5556decd1c36b2426be6ccf5fcfe4
Jasmine Bae <jasmine.bae@berkeley.edu>
2020-06-27 17:33:52 -0700

    Updates

---
+++ precache-manifest.17c0294beedc1d527f6fab6ee6cb0c48.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9be7526569f1a3920e0fb3b55fd70911",
+    "url": "/justamity/index.html"
+  },
+  {
+    "revision": "27ed6b5d169291119ef5",
+    "url": "/justamity/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "43694338d67d44059022",
+    "url": "/justamity/static/js/2.a47ae742.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/justamity/static/js/2.a47ae742.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "27ed6b5d169291119ef5",
+    "url": "/justamity/static/js/main.9631ad38.chunk.js"
+  },
+  {
+    "revision": "5526a3ba34e21941b317",
+    "url": "/justamity/static/js/runtime-main.c2860ad6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/justamity/static/media/logo.5d5d9eef.svg"
+  }
+]);
