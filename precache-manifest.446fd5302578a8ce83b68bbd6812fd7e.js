7608351c8cd129c9cd7d245fd7879290b1a3ac9a
Jasmine Bae <jasmine.bae@berkeley.edu>
2020-08-04 19:58:59 -0700

    Updates

---
+++ precache-manifest.446fd5302578a8ce83b68bbd6812fd7e.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e4d150952f918b69a3322013915a66d3",
+    "url": "/justamity/index.html"
+  },
+  {
+    "revision": "bd8fa07a92d278f1d85b",
+    "url": "/justamity/static/css/main.f8ef9499.chunk.css"
+  },
+  {
+    "revision": "0508340fa536afbe4335",
+    "url": "/justamity/static/js/2.fe25dfc3.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/justamity/static/js/2.fe25dfc3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bd8fa07a92d278f1d85b",
+    "url": "/justamity/static/js/main.824fd7c5.chunk.js"
+  },
+  {
+    "revision": "5526a3ba34e21941b317",
+    "url": "/justamity/static/js/runtime-main.c2860ad6.js"
+  },
+  {
+    "revision": "3737bdfcb1f8403af9b2d49dcf78fe0b",
+    "url": "/justamity/static/media/homeBackground.3737bdfc.png"
+  },
+  {
+    "revision": "0addf8c86e91673a452987d9d2677092",
+    "url": "/justamity/static/media/logo3.0addf8c8.svg"
+  },
+  {
+    "revision": "dce4df7f26b1365995df5bc19516e8b3",
+    "url": "/justamity/static/media/mail.dce4df7f.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/justamity/precache-manifest.4a2b2346f7b2183d09e98c0635476651.js"
+  "/justamity/precache-manifest.446fd5302578a8ce83b68bbd6812fd7e.js"
 );
 
 self.addEventListener('message', (event) => {
